47b15e7865f67db913f933d7df6dfb416f02bf2c
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-08-31 15:52:26 -0500

    Added Crashing
    Landing too hard will cause the ship to crash

--- Assets/LandingDetectionManager.cs
+++ Assets/LandingDetectionManager.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class LandingDetectionManager : MonoBehaviour {
 
+    public float maxLandingSpeed; //over what speed does a ship crash at
+    [HideInInspector]
     public bool frontLanded; //the bool connected with the front
+    [HideInInspector]
     public bool endLanded; // the bool connected with the back
     bool hasFinished; //the activation of the win condition, prevents the effect of winning from happening multiple times
 	
@@ -16,8 +19,19 @@ public class LandingDetectionManager : MonoBehaviour {
             {
                 Debug.Log("YOU'VE LANDED!");
                 //other win conditions
+                Crashing();
+                print(this.GetComponent<Rigidbody2D>().velocity.y);
                 hasFinished = true;
             }
         }
 	}
+
+    void Crashing()
+    {
+        if (Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.y) > maxLandingSpeed)
+        {
+            Debug.LogError("WE'VE Crashed!");
+            this.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+        }
+    }
 }

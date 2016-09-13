79ffe194c2dacb727bbd4508d9a6bb10c0e037be
gebharsj <gebharsj@gmatc.matc.edu>
2016-09-13 16:49:43 -0500

    added pause menu code

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    bool paused = false;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        //toggles the pause menu gameobject
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            paused = togglePause();
+            if (paused)
+            {
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+
+    bool togglePause()      //Sets the time scale to pause the game
+    {
+        if (Time.timeScale == 0) 
+        {
+            Time.timeScale = 1;     //unpauses game
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0;     //pauses game
+            return (true);
+        }
+    }
+}

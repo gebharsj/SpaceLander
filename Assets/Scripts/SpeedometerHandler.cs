8e591ad5800bc9ebf29e3a7c3d8ed34c42bcaf2d
gebharsj <gebharsj@gmatc.matc.edu>
2016-09-14 08:07:33 -0500

    Added the speedometer

---
+++ Assets/Scripts/SpeedometerHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpeedometerHandler : MonoBehaviour {
+
+    public GameObject player;       //The player
+    public Text speedText;
+    Rigidbody2D shipRB;             //The RigidBody2D on the ship
+
+	// Use this for initialization
+	void Start () {
+        shipRB = player.GetComponent<Rigidbody2D>();        //Cache reference to player's rigidbody
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        DisplaySpeed();
+    }
+
+    void DisplaySpeed()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, -Mathf.Abs(shipRB.velocity.magnitude * 15));
+        speedText.text = Mathf.Abs(shipRB.velocity.magnitude * 6).ToString();
+    }    
+}

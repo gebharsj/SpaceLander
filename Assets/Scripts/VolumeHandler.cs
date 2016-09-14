bb4645d778cad431fb3e481e449599955a7f61b8
gebharsj <gebharsj@gmatc.matc.edu>
2016-09-14 09:11:45 -0500

    added volume handler

---
+++ Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeHandler : MonoBehaviour {
+
+    public AudioSource uISource;
+    public AudioSource musicSource;
+
+    public string uiVolume;
+    public string musicVolume;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uISource.volume = PlayerPrefs.GetFloat(uiVolume);
+        musicSource.volume = PlayerPrefs.GetFloat(musicVolume);
+    }
+}

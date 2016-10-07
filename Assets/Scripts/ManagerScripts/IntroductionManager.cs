f2b77ab7743709e0079a9714c66b8f0359a70633
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-10-07 11:04:44 -0500

    Added advancing/regressing buttons

--- Assets/Scripts/ManagerScripts/IntroductionManager.cs
+++ Assets/Scripts/ManagerScripts/IntroductionManager.cs
@@ -7,48 +7,40 @@ public class IntroductionManager : MonoBehaviour {
     [Tooltip("The panels you want to display in the intro.")]
     public GameObject[] panels;
     [Tooltip("Delay before next panel can be seen.")]
-    public int clickDelay = 2;          //the delay between being able to skip through them all
-    int cnt;                            //a counter to keep track of the currrent panel
+    public float clickDelay = 2;          //the delay between being able to skip through them all
+    [HideInInspector]
+    public int cnt = -1;                //a counter to keep track of the currrent panel
     bool isDelayed;                     //bool to handle the click delay
     // Use this for initialization
     void Start()
     {
-        AdvanceScene();
+        AdvanceScene(true);                                                     //setups on the panels correctly on start
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.anyKeyDown)  // When any key is pressed and let go...
-        {
-            StartCoroutine(DelayClick());
-        }
-    }
-
-    IEnumerator DelayClick()
+    public IEnumerator DelayClick(bool isAdvance)                               //based upon bool
     {
         if (!isDelayed)
         {
             isDelayed = true;                                                  //prevents delay from happening twice
-            AdvanceScene();                                                     //display next panel
+            AdvanceScene(isAdvance);                                            //display next panel
             yield return new WaitForSeconds(clickDelay);                       //prevents spam clicking
             isDelayed = false;
         }
     }
 
-    void AdvanceScene()
+    void AdvanceScene(bool isAdvance)
     {
+        if (isAdvance)          //if true, go forward
+            cnt++;
+        else
+            cnt--;              //if false, go back
+
         foreach (GameObject panel in panels) //turn off all the panels
         {
             panel.SetActive(false);
         }
 
-        panels[cnt].SetActive(true);    //turn on the one we want
-
-        cnt++;                          //prepare for the next panel
-
-        if (cnt >= panels.Length)       //if we'll activated all the panels
-            SceneManager.LoadScene(1);  //load the first level of the game
+        panels[cnt].SetActive(true);    //turn on the one we want        
     }
 }
 

bda985abb8cc0065b0aefe989ff5ff81c32d3f39
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-10-14 11:27:07 -0500

    Started BasicUtilities

---
+++ Assets/BasicUtilities.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class BasicUtilities<T> : MonoBehaviour
+{
+    static List<T> onlyOnceValues = new List<T>();
+
+    public static int checkIncrement(int value, T[] array)
+    {
+        value++;
+
+        if (value >= array.Length)
+            value = 0;
+
+        return value;
+    }
+
+    public static int checkIncrement(int value, List<T> array)
+    {
+        value++;
+
+        if (value >= array.Count)
+            value = 0;
+
+        return value;
+    }
+
+    public static int checkIncrement(int value, int maxValue)
+    {
+        value++;
+
+        if (value >= maxValue)
+            value = 0;
+
+        return value;
+    }
+
+    public static bool onlyOnce(T checkValue)
+    {
+        return uniqInsert(checkValue, onlyOnceValues);
+    }
+
+    public static void resetOnce(T checkValue)
+    {
+        if (onlyOnceValues.Contains(checkValue))
+            onlyOnceValues.Remove(checkValue);
+        else
+            Debug.LogError("You tried to remove something that hasn't been added.");
+    }
+
+    public static void updateCoroutine(float waitTime)
+    {
+        StartCoroutine(Delay(waitTime));
+    }
+
+    static IEnumerator Delay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    public static bool uniqInsert(T checkValue, List<T> valueList)
+    {
+        if (!valueList.Contains(checkValue))
+        {
+            onlyOnceValues.Add(checkValue);
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ Assets/Scripts/UI/TestDriver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDriver : MonoBehaviour {
+
+    int cnt = 0;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cnt++;
+
+        if (BasicUtilities<string>.onlyOnce("Hello"))
+            print("Cheese");
+
+        if (cnt > 100)
+        {
+            BasicUtilities<string>.resetOnce("Hello");
+            cnt = 0;
+            print("reset");
+        }
+
+	}
+}

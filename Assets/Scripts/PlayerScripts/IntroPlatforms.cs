4342de7c69b871ad6e547d1754aaef17714f41b5
erinf <erincfink@gmail.com>
2016-10-18 21:52:53 -0500

    Wind speeds

--- Assets/Scripts/PlayerScripts/IntroPlatforms.cs
+++ Assets/Scripts/PlayerScripts/IntroPlatforms.cs
@@ -6,6 +6,6 @@ public class IntroPlatforms : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        gameObject.GetComponent<Image>().color = Color.red;
+        gameObject.GetComponent<Image>().color = Color.red;     // Change the platform red when landed on
     }
 }

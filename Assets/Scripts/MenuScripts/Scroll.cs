9a8ea7c1ebc621c673e81593adff2accf58762d0
erinf <erincfink@gmail.com>
2016-10-25 13:56:02 -0500

    Credits added
    Scrolls on timer, returns to menu

---
+++ Assets/Scripts/MenuScripts/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scroll : MonoBehaviour {
+
+    public float scrollSpeed = 1;
+    public int sceneTimer = 13;
+
+    void Start() {
+        StartCoroutine(scroll());
+    }
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(Vector3.up * scrollSpeed);
+	}
+
+    IEnumerator scroll() {
+        yield return new WaitForSeconds(sceneTimer);
+        SceneManager.LoadScene(0);
+    }
+}
